5fcb8fc6ddfcf15f84fd84aec8236bcc5d75edc6
Muhammad Irfan <funnyirfan321@gmail.com>
2021-06-09 16:17:37 +0700

    changes

--- app/Controllers/Http/CategoryServiceController.js
+++ app/Controllers/Http/CategoryServiceController.js
@@ -22,18 +22,18 @@ class CategoryServiceController {
    */
   async index({ params, request, response }) {
     try {
-      const payload = request.all();
-      const page = parseInt(payload.page) || 1;
-      const limit = parseInt(payload.limit) || 5;
-      const members = await Category.query()
-        .where("id", params.categories_id)
-        .orWhere("category_slug", params.categories_id)
-        .with("services", (builder) => {
-          builder.forPage(page, limit);
-        })
-        .first();
-
-      return response.status(200).json(members);
+      // const payload = request.all();
+      // const page = parseInt(payload.page) || 1;
+      // const limit = parseInt(payload.limit) || 5;
+      // const members = await Category.query()
+      //   .where("id", params.categories_id)
+      //   .orWhere("category_slug", params.categories_id)
+      //   .with("services", (builder) => {
+      //     builder.forPage(page, limit);
+      //   })
+      //   .first();
+      //
+      // return response.status(200).json(members);
     } catch (e) {
       return response.status(500).json({
         error: e,
